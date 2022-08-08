6fc0f5f5d773a58cfe0633d4c3cac92980d2a838
Luis Hermida <110241973+LUKKA27@users.noreply.github.com>
2022-08-07 21:37:46 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function ciclo1(num1, num2, num3  ) {
+	if (num2 > num1) {
+		console.log("Este no es el numero del medio")
+	} else if (num3 > num2) {
+		console.log("Como num3 es mayor que num2 y menor que num1 el numero de en medio es ", (num3))
+	};
+
+};
+ciclo1(10,4,7)
+//funcion flecha
+
+const ciclo2=(num1, num2, num3)=>{
+		if (num2 > num1) {
+		console.log("Este no es el numero del medio")
+	} else if (num3 > num2) {
+		console.log("Como num3 es mayor que num2 y menor que num1 el numero de en medio es ", (num3))
+	};
+
+}
+ciclo2(1,4,4)
+
